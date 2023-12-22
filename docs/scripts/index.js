f6c177199ac51da844f3dd056ba65f2c81ea1247
Ndimah Mike <ndimah22@protonmail.com>
2023-12-22 16:53:10 +0100

    feat(interactions): implement micro-actions in the page
    the following micro-features has been implemented:
    - theme switch
    - invoice preview
    - invoice form step switch
    - drawer reveal
    - deletion dialog reveal
    - invoice form step indicator update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,5 +1,84 @@
-import StepByStep from "./step-by-step.js"
-import Datepicker from "./datepicker.js"
+/*jslint browser*/
+import StepByStep from "./step-by-step.js";
+import Datepicker from "./datepicker.js";
 
-StepByStep.define()
-Datepicker.define()
+const config = {};
+let currentTheme = (
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light"
+);
+
+config.themeSwitches = {dark: "light", light: "dark"};
+config.drawer = document.querySelector(".drawer");
+config.invoiceForm = document.querySelector("form#invoice_form");
+config.invoiceDetails = document.querySelector(".invoice__details");
+config.stepIndicators = document.querySelectorAll(".step-indicator > li");
+config.nextFormStep = document.querySelector("#next_step");
+config.prevFormStep = document.querySelector("#prev_step");
+config.dialog = document.querySelector("dialog");
+
+config.invoiceForm.firstElementChild.addEventListener(
+    "indexupdated",
+    function ({detail}) {
+        const {current, previous} = detail;
+        if (current > 0) {
+            config.prevFormStep.disabled = false;
+        } else {
+            config.prevFormStep.disabled = true;
+        }
+        if (current >= config.stepIndicators.length - 1) {
+            config.nextFormStep.disabled = true;
+        } else {
+            config.nextFormStep.disabled = false;
+        }
+        if (previous > current) {
+            config.stepIndicators[previous].classList.remove("step-active");
+        }
+        config.stepIndicators[current].classList.add("step-active");
+    },
+    false
+);
+
+document.body.addEventListener("click", function ({target}) {
+    const root = document.documentElement;
+    if (target.classList.contains("theme-switch")) {
+        root.dataset.theme = config.themeSwitches[currentTheme];
+        currentTheme = config.themeSwitches[currentTheme];
+    }
+    if (target.classList.contains("invoice__summary")) {
+        target.closest("step-by-step").nextStep();
+    }
+    if (target.classList.contains("back")) {
+        target.closest("step-by-step").previousStep();
+    }
+    if (target.id === "new_invoice") {
+        document.body.dataset.drawer = "show";
+    }
+}, false);
+config.drawer.addEventListener("click", function ({target}) {
+    if (target.classList.contains("cancel")) {
+        delete document.body.dataset.drawer;
+        delete config.drawer.dataset.edit;
+    }
+    if (target.id === "next_step") {
+        config.invoiceForm.firstElementChild.nextStep();
+    }
+    if (target.id === "prev_step") {
+        config.invoiceForm.firstElementChild.previousStep();
+    }
+}, false);
+config.invoiceDetails.addEventListener("click", function ({target}) {
+    if (target.classList.contains("back")) {
+        config.invoiceDetails.closest("step-by-step").previousStep();
+    }
+    if (target.classList.contains("btn-edit")) {
+        config.drawer.dataset.edit = "";
+        document.body.dataset.drawer = "show";
+    }
+    if (target.classList.contains("btn-danger")) {
+        config.dialog.showModal();
+    }
+}, false);
+StepByStep.define();
+Datepicker.define();
